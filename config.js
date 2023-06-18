ee67c5529b7f5effa99b5565b5c9088611fd8bcd
Colby Haskell <cchaskel@usc.edu>
2023-06-18 19:58:19 -0400

    update conn string names

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const env = process.env.NODE_ENV || 'development';
 const credentials = {
-    sendgrid: {
-        user: process.env.SENDGRID_USER,
-        pass: process.env.SENDGRID_PASS,
-    },
-    mongo: {
-        connectionString: process.env.MONGO_CONNECTION_STRING,
-    }
-}
+  sendgrid: {
+    user: process.env.CUSTOMCONNSTR_SENDGRID_USER,
+    pass: process.env.CUSTOMCONNSTR_SENDGRID_PASS,
+  },
+  mongo: {
+    connectionString: process.env.CUSTOMCONNSTR_MONGO_CONNECTION_STRING,
+  },
+};
 
 module.exports = { credentials };
