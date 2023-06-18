b9729331b145d0ea7bd4e86630b7841a631d84f2
Colby Haskell <cchaskel@usc.edu>
2023-06-18 18:38:22 -0400

    Switch to env variables

--- config.js
+++ config.js
@@ -1,4 +1,12 @@
 const env = process.env.NODE_ENV || 'development';
-const credentials = require(`./.credentials.${env}`);
+const credentials = {
+    sendgrid: {
+        user: process.env.SENDGRID_USER,
+        pass: process.env.SENDGRID_PASS,
+    },
+    mongo: {
+        connectionString: process.env.MONGO_CONNECTION_STRING,
+    }
+}
 
 module.exports = { credentials };
